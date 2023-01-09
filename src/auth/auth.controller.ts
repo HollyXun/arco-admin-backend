54c9362795eeaa380a671348419ab178bf4598f1
oxygen <lixun.hu@foxmail.com>
2023-01-09 21:07:09 +0800

    jwt本地验证

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,7 +14,7 @@ import {
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {ApiBearerAuth, ApiOperation, ApiTags} from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { LoginDto } from './dto/login-auth.dto';
 
@@ -27,6 +27,14 @@ export class AuthController {
   @UseInterceptors(ClassSerializerInterceptor)
   @Post('login')
   async login(@Body() user: LoginDto, @Req() req) {
+    return req.user;
+  }
+
+  @ApiOperation({ summary: '获取用户信息' })
+  @ApiBearerAuth() // swagger文档设置token
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  getUserInfo(@Req() req) {
     return req.user;
   }
 }
--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,16 @@
-export class CreateAuthDto {}
+import {ApiProperty} from "@nestjs/swagger";
+import {IsNotEmpty} from "class-validator";
+
+export class CreateAuthDto {
+    @ApiProperty({ description: '用户名' })
+    @IsNotEmpty({ message: '请输入用户名' })
+    username: string;
+
+    @ApiProperty({ description: '密码' })
+    @IsNotEmpty({ message: '请输入密码' })
+    password: string;
+
+    @ApiProperty({ description: '类型' })
+    @IsNotEmpty({ message: '请选择身份' })
+    role: string;
+}
